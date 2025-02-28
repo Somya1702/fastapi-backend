04a5636172169783345b3eac2ef258bc1aa9aa29
CA Somya JAIN <somya.dco@Gmail.com>
2025-02-28 23:08:59 +0530

    Disabled FastAPI docs, only showing SCN REPLY

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)  # Disable API docs
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root():
+async def home():
     return "<h1>SCN REPLY</h1>"
